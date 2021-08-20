4e0298c640fa3b24085a55721ca7ba034abb5567
unknown <rakibulhasan.seu.cse@gmail.com>
2021-08-21 02:29:59 +0600

    memory cost calculation done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// extra memory for 16gb
+document.getElementById('extra-memory-btn-16gb').addEventListener('click', function(){
+    document.getElementById('extra-memory-cost').innerText = '180';
+});
+// extra memory for 16gb
+document.getElementById('extra-memory-btn-8gb').addEventListener('click', function(){
+    document.getElementById('extra-memory-cost').innerText = '0';
+});
