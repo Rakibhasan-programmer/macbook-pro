a84cbaaf642c00e00c16821f995a2906c1ebba20
unknown <rakibulhasan.seu.cse@gmail.com>
2021-08-21 15:02:48 +0600

    project done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function costCalculation(id, price){
     document.getElementById(id).innerText = price;
     //function call
     calculateTotalPrice();
-}
+};
 // calculate total
 function calculateTotalPrice(){
     // best price
@@ -19,46 +19,13 @@ function calculateTotalPrice(){
     const deliveryCost = document.getElementById('delivery-cost');
     const deliveryCostAmount = parseInt(deliveryCost.innerText);
     // total calculation
-    const totalPrice = bestPriceAmount+memoryCostAmount+storageCostAmount+deliveryCostAmount;
+    const totalPrice = bestPriceAmount+memoryCostAmount+storageCostAmount +deliveryCostAmount;
     // displaying total price and total
     document.getElementById('total-cost').innerText = totalPrice;
     document.getElementById('grand-total').innerText = totalPrice;
-}
-
-// memory 16gb eventlistener
-document.getElementById('extra-memory-btn-16gb').addEventListener('click', function(){
-    costCalculation('extra-memory-cost', 180);
-});
-// memory 8gb eventlistener
-document.getElementById('extra-memory-btn-8gb').addEventListener('click', function(){
-    costCalculation('extra-memory-cost', 0);
-});
-
-// storage 256 eventlistener
-document.getElementById('storage-btn-256gb').addEventListener('click', function(){
-    costCalculation('extra-storage-cost', 0);
-});
-// storage 512 eventlistener
-document.getElementById('storage-btn-512gb').addEventListener('click', function(){
-    costCalculation('extra-storage-cost', 100);
-});
-// storage 1Tb eventlistener
-document.getElementById('storage-btn-1tb').addEventListener('click', function(){
-    costCalculation('extra-storage-cost', 180);
-});
-
-
-// delivery cost free eventlistener
-document.getElementById('delivery-cost-free').addEventListener('click', function(){
-    costCalculation('delivery-cost', 0);
-});
-// delivery cost included eventlistener
-document.getElementById('delivery-cost-included').addEventListener('click', function(){
-    costCalculation('delivery-cost', 20);
-});
-
-//apply promocode
-document.getElementById('promo-code-btn').addEventListener('click', function(){
+};
+// apply promocode
+function promoCode(){
     const promoCode = 'stevekaku';
     // taking input value
     const promoCodeInput = document.getElementById('promoCode-input');
@@ -72,5 +39,39 @@ document.getElementById('promo-code-btn').addEventListener('click', function(){
         const total = (newTotalCost*80)/100;
         document.getElementById('grand-total').innerText = total;
     }
+    //empty input
     promoCodeInput.value = '';
+};
+
+// memory 16gb btn eventlistener
+document.getElementById('extra-memory-btn-16gb').addEventListener('click', function(){
+    costCalculation('extra-memory-cost', 180);
+});
+// memory 8gb btn eventlistener
+document.getElementById('extra-memory-btn-8gb').addEventListener('click', function(){
+    costCalculation('extra-memory-cost', 0);
+});
+// storage 256 btn eventlistener
+document.getElementById('storage-btn-256gb').addEventListener('click', function(){
+    costCalculation('extra-storage-cost', 0);
+});
+// storage 512gb btn eventlistener
+document.getElementById('storage-btn-512gb').addEventListener('click', function(){
+    costCalculation('extra-storage-cost', 100);
+});
+// storage 1Tb btn eventlistener
+document.getElementById('storage-btn-1tb').addEventListener('click', function(){
+    costCalculation('extra-storage-cost', 180);
+});
+// delivery cost free btn eventlistener
+document.getElementById('delivery-cost-free').addEventListener('click', function(){
+    costCalculation('delivery-cost', 0);
+});
+// delivery cost included btn eventlistener
+document.getElementById('delivery-cost-included').addEventListener('click', function(){
+    costCalculation('delivery-cost', 20);
+});
+//promocode btn eventlistener
+document.getElementById('promo-code-btn').addEventListener('click', function(){
+    promoCode();
 });
